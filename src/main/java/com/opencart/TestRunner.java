f0dfb606ee2d8599ac7c9296b08d893db6ff2ca1
MarianaUserGit <maroleinic@gmail.com>
2023-06-17 00:26:31 +0300

    Implement the Driver Manager and configure the Selenium dependencies

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,36 @@
 package com.opencart;
 
+import com.opencart.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+
+//        Define a driver object that will be used for future actions.
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java");
+
+        String currentWindowName = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        driver.get("https:/chromedriver.chromium.org/downloads");
+        Thread.sleep(5000);
+
+        driver.close();
+
+        driver.switchTo().window(currentWindowName);
+        Thread.sleep(1000);
+
+        driver.get("https://diez.md/");
+
+        driver.quit();
+
+        System.out.println("The execution was finished");
+
+//        DriverManager driverManager = DriverManager.getInstance();
+//        WebDriver driver2 = driverManager.getDriver();
     }
 }
